e6a64d687ec4921ca57f379864af38e1c34103b5
lightning1141 <lightning1141@gmail.com>
2017-05-06 10:14:54 +0800

    build verbosely

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ GCC=$ARCH-elf-gcc
 BIN=$ARCH-elf-binutils
 GDB=$ARCH-elf-gdb
 
-brew install $BIN
-brew install $GCC
-brew install $GDB
+brew install --verbose $BIN
+brew install --verbose $GCC
+brew install --verbose $GDB
